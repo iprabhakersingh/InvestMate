3c65165b21ba8e75849614d5713ac97ae0acbf86
Prabhaker Singh <iprabhakersingh@gmail.com>
2025-11-19 23:23:43 +0530

    Fast Optimize

--- dashboard/src/components/Dashboard.js
+++ dashboard/src/components/Dashboard.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import { GeneralContextProvider } from "./GeneralContext";
-
 import WatchList from "./WatchList";
 import Summary from "./Summary";
 import Holdings from "./Holdings";
@@ -9,34 +9,18 @@ import Positions from "./Positions";
 import Funds from "./Funds";
 
 const Dashboard = () => {
-  const [activeTab, setActiveTab] = useState("summary");
-
   return (
     <GeneralContextProvider>
       <div className="dashboard-container">
-        
-        {/* LEFT SIDE WATCHLIST */}
         <WatchList />
-
-        {/* RIGHT SIDE CONTENT */}
         <div className="content">
-          
-          {/* TAB BUTTONS */}
-          <div className="tabs">
-            <button onClick={() => setActiveTab("summary")}>Summary</button>
-            <button onClick={() => setActiveTab("holdings")}>Holdings</button>
-            <button onClick={() => setActiveTab("positions")}>Positions</button>
-            <button onClick={() => setActiveTab("orders")}>Orders</button>
-            <button onClick={() => setActiveTab("funds")}>Funds</button>
-          </div>
-
-          {/* TAB CONTENT */}
-          {activeTab === "summary" && <Summary />}
-          {activeTab === "holdings" && <Holdings />}
-          {activeTab === "positions" && <Positions />}
-          {activeTab === "orders" && <Orders />}
-          {activeTab === "funds" && <Funds />}
-
+          <Routes>
+            <Route exact path="/" element={<Summary />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/holdings" element={<Holdings />} />
+            <Route path="/positions" element={<Positions />} />
+            <Route path="/funds" element={<Funds />} />
+          </Routes>
         </div>
       </div>
     </GeneralContextProvider>
@@ -44,4 +28,3 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
-
